f57cae07d563f02e7315c8dd1c85fdbdbdac0a94
Rivaaas <rivaaas.v@gmail.com>
2022-11-26 11:33:19 -0300

    Implementando REDUX

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 
 
+//REDUX
+import { Provider } from 'react-redux'
+import store from './store'
+
 function App() {
   return (
     <>
       <Header />
       <div className="container mt-5">
         <Router>
-          <Routes>
-            <Route exact path="/" element={<Producto/>} />
-            <Route exact path="/producto/nuevo" element={<NuevoProducto/>} />
-            <Route exact path="/producto/editar/:id" element={<EditarProducto />} />
-          </Routes>
+          <Provider store={store}>
+            <Routes>
+              <Route exact path="/" element={<Producto />} />
+              <Route exact path="/producto/nuevo" element={<NuevoProducto />} />
+              <Route exact path="/producto/editar/:id" element={<EditarProducto />} />
+            </Routes>
+          </Provider>
         </Router>
       </div>
     </>
---
+++ src/actions/productoActions.js
@@ -0,0 +1,18 @@
+import {
+  AGREGAR_PRODUCTO,
+  AGREGAR_PRODUCTO_EXITO,
+  AGREGAR_PRODUCTO_ERROR
+} from '../types'
+
+
+//Crear nuevos productos
+
+export function crearNuevoProductoAction(producto) {
+  return (dispatch) => {
+    dispatch( agregarProducto() );
+  }
+}
+
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO
+})
